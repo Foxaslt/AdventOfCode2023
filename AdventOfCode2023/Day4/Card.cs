964e0ecbdd1d672d5cb0655427773aefb58feb18
Romualdas Opulskis <romualdas.opulskis@transunion.co.uk>
2023-12-17 23:34:42 +0200

    Day4

---
+++ AdventOfCode2023/Day4/Card.cs
@@ -0,0 +1,20 @@
+﻿namespace Day4
+{
+    internal class Card
+    {
+        private string name;
+        List<int> winning = new List<int>();
+        List<int> myNumbers = new List<int>();
+
+        public Card(string s)
+        {
+            var data = s.Split(':');
+            name = data[0].Trim();
+
+            var numbers = data[1].Split('|');
+            winning.AddRange(numbers[0].Trim().Split(' ').Where(n => !string.IsNullOrEmpty(n)).Select(int.Parse));
+            myNumbers.AddRange(numbers[1].Trim().Split(' ').Where(n => !string.IsNullOrEmpty(n)).Select(int.Parse));
+        }
+
+    }
+}
---
+++ AdventOfCode2023/Day4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rawData = File.ReadAllLines("RawData.txt");
+
+            List<Card> cards = rawData.Select(s => new Card(s)).ToList();
+
+            //Console.WriteLine($"Part 1: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
