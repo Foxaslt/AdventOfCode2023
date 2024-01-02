2fb064773aebb9a99d7582c36479f35a8554e993
Romualdas Opulskis <romualdas.opulskis@transunion.co.uk>
2024-01-02 11:38:12 +0200

    Day6

---
+++ AdventOfCode2023/Day6/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Race> races = new List<Race>();
+            var rawData = File.ReadAllLines("RawData.txt");
+            var time = rawData[0].Split(':')[1].Trim().Split(' ').Where(m => !string.IsNullOrWhiteSpace(m));
+            var distance = rawData[1].Split(':')[1].Trim().Split(' ').Where(m => !string.IsNullOrWhiteSpace(m));
+        }
+    }
+}
